8bbc40cbfd0cc17929b46983b2f9d4195259395f
Brennan Cottrell <whiteeaglelane@gmail.com>
2023-10-29 11:21:55 -0400

    hand written conversion for pqxx row to generated struct

--- cpp/query.cpp
+++ cpp/query.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
 #include "procedures.h"
+#include "types.h"
 #include "hidden.h"
+
 
 int main()
 {
@@ -9,10 +12,20 @@ int main()
     pqxx::connection c{conString.str()};
     pqxx::work txn{c};
 
-    pqxx::result r = get_author_name_like(txn, "Cor");
+    pqxx::result r = get_authors_over_age(txn, 85);
+    std::vector<author> vec; 
     for (auto row : r)
     {
-        std::cout << row["firstName"] << " " << row["lastName"] << "\n";
+        author a; 
+        a.id = std::stoi(row["id"].c_str());
+        a.firstname = row["firstName"].c_str();
+        a.lastname = row["lastName"].c_str();
+        a.age = std::stoi(row["age"].c_str());
+        vec.push_back(a);
+    }
+    for (auto i : vec) 
+    {
+        std::cout << i.firstname << " " << i.lastname << "\n";
     }
 
     return 0;
